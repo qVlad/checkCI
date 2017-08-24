ac1acca9cec711fedb4f9e14ec885abcbc25b6d4
qVlad <vipryn@gmail.com>
2017-08-24 06:22:37 +0300

    Init

---
+++ build/CMakeFiles/hello_SRC.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/hello_SRC.dir/src/main.cpp.o"
+  "hello_SRC.pdb"
+  "hello_SRC"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/hello_SRC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
